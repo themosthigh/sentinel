c60c66654e0ffa95a0e7a892f6eaff40326a33ae
TheMostHigh <tallestlegacy@gmail.com>
2026-02-28 14:06:05 +0300

    feat: Init tailwind css on blank canvas

---
+++ src/app/(frontend)/(home)/page.tsx
@@ -0,0 +1,3 @@
+export default async function HomePage() {
+  return <div className="p-2 bg-red-400">lorem ipsum site dolor amet</div>
+}
--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -7,7 +7,8 @@ import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
-import './custom.scss'
+
+import '@/styles/payloadStyles.css'
 
 type Args = {
   children: React.ReactNode
